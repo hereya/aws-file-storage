81817848110805cdff630b785811faf47816b2c7
Jonatan Sombié <jonatan.sombie@gmail.com>
2026-03-31 11:44:24 +0200

    Initial commit

---
+++ test/hereya-aws-file-storage.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HereyaAwsFileStorage from '../lib/hereya-aws-file-storage-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hereya-aws-file-storage-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HereyaAwsFileStorage.HereyaAwsFileStorageStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
